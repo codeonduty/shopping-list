182dfc2bc9b7e655c07f9d8d20b217c0d491d68a
vctrblck <sphesihlentshingila@protonmail.com>
2022-06-03 15:25:20 +0200

    Added tests + CI

--- server.js
+++ server.js
@@ -1,31 +1,20 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const fileUpload = require("express-fileupload");
-const cookieParser = require("cookie-parser");
-const path = require("path");
+// server.js
 
-const app = express();
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors());
-app.use(
-  fileUpload({
-    useTempFiles: true,
-  })
-);
+// Libraries:
 
-// Routes
-app.use("/user", require("./routes/userRouter"));
-app.use("/api", require("./routes/categoryRouter"));
-app.use("/api", require("./routes/upload"));
-app.use("/api", require("./routes/productRouter"));
-app.use("/api", require("./routes/paymentRouter"));
+require('dotenv').config();
 
-// Connect to mongodb
+// Modules:
+
+const database = require('./database');
+const application = require('./application');
+
+// Code:
+
 const URI = process.env.MONGODB_URL;
-mongoose.connect(
+const PORT = process.env.PORT || 5000;
+
+database.connect(
   URI,
   {
     useCreateIndex: true,
@@ -35,18 +24,10 @@ mongoose.connect(
   },
   (err) => {
     if (err) throw err;
-    console.log("Connected to MongoDB");
+    console.log('Connected to MongoDB');
   }
 );
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-}
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log("Server is running on port", PORT);
+application.listen(PORT, () => {
+  console.log('Server is running on port', PORT);
 });
