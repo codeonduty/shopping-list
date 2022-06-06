aacb033ce2adb25c4e3967e88907ed8b2777ef97
HactorPK <handsomesurprisem@gmail.com>
2022-06-06 11:34:37 +0200

    Added light and dark mode on CSS files

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,36 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import {DataProvider} from './GlobalState'
 import Header from './components/headers/Header'
 import MainPages from './components/mainpages/Pages'
-
+import {useState} from "react";
+import {createContext} from "react";
+import ReactSwitch from "react-switch"
+export const ThemeContext = createContext(null);
 
 function App() {
+
+  const[theme, setTheme] = useState("dark");
+  
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light")); 
+  }
   return (
+    <ThemeContext.Provider value = { { theme, toggleTheme} }>
     <DataProvider>
       <Router>
-        <div className="App">
+      
+        <div className="App" id = {theme}>
           <Header />
           <MainPages />
+          <div className = 'switch'>
+            <label> {theme ==="light" ? "Light Mode" : "Dark Mode"}</label>
+          <ReactSwitch onChange={toggleTheme} checked={theme==="dark"}/>
+          </div>
         </div>
+       
+        
       </Router>
     </DataProvider>
+    </ThemeContext.Provider>
   );
 }
 
--- client/src/components/mainpages/auth/Login.js
+++ client/src/components/mainpages/auth/Login.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 import axios from 'axios'
 
+
 function Login() {
     const [user, setUser] = useState({
         email:'', password: ''
@@ -28,7 +29,6 @@ function Login() {
     return (
         <div className="login-page">
             <form onSubmit={loginSubmit}>
-                <h2>Login</h2>
                 <input type="email" name="email" required
                 placeholder="Email" value={user.email} onChange={onChangeInput} />
 
